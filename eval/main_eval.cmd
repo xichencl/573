a4c6c95ca2b9cb943e2f25688c3d0e3cab3e3bef
mblac6 <mblac6@uw.edu>
2017-04-20 16:06:54 -0700

    added condor

---
+++ eval/main_eval.cmd
@@ -0,0 +1,10 @@
+executable = main_eval.sh
+getenv     = true
+input   = /dev/null
+output  = /dev/null
+error      = main_eval.error
+log        = main_eval.log
+notification = complete
+transfer_executable = false
+request_memory = 4*1024
+queue
