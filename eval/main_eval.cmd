a115edcc24386a1c877a8f3b9aba0aff18ba38c9
mblac6 <mblac6@uw.edu>
2017-04-20 17:44:32 -0700

    first sentence ROUGE test

--- eval/main_eval.cmd
+++ eval/main_eval.cmd
@@ -1,7 +1,7 @@
 executable = main_eval.sh
 getenv     = true
 input   = /dev/null
-output  = /dev/null
+output  = main_eval.out
 error      = main_eval.error
 log        = main_eval.log
 notification = complete
