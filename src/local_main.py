ef62d1e7f11f33dfda0a388caa668a71f2bff437
mblac6 <mblac6@uw.edu>
2017-05-04 18:20:30 -0700

    added local main for reference

---
+++ src/local_main.py
@@ -0,0 +1,38 @@
+import json
+import ContentSelector
+import ContentRealizer
+import ContentSorter
+import sys
+import re
+import random
+
+gold = json.load(open(sys.argv[1], 'r'))
+train_documents = json.load(open(sys.argv[2], 'r'))
+test_documents = json.load(open(sys.argv[3], 'r'))
+proc_test = json.load(open(sys.argv[4], 'r'))
+
+selector = ContentSelector.ContentSelector()
+selector.train(train_documents, gold)
+
+sorter = ContentSorter.ContentSorter();
+
+realizer = ContentRealizer.ContentRealizer()
+
+index = 0
+for event in test_documents:
+    index += 1
+    an_event = test_documents[event]
+    proc_event = proc_test[event]
+    print("Testing " + str(index) + '/' + str(len(test_documents.keys())))
+    results = selector.test(an_event, proc_event, event, 10)
+    sorter.sort(results);
+    picked = realizer.realize(results, 100)
+    summary = re.sub('\W+', ' ', ' '.join(picked))
+    letter = event[-1]
+
+    if 'Group' in event:
+        out = open('/Users/mackie/PycharmProjects/573/outputs/D2/' + event, 'w')
+    else:
+        out = open('/Users/mackie/PycharmProjects/573/outputs/D2/' + event[:-1] + '-A.M.100.' + letter + '.A', 'w')
+    out.write(summary)
+
