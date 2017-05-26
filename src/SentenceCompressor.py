660e51dec5f1a9357abfe002b380b6cf2fa2f987
mblac6 <mblac6@uw.edu>
2017-05-26 09:52:57 -0700

    minimal sentence compression

--- src/SentenceCompressor.py
+++ src/SentenceCompressor.py
@@ -1,8 +1,17 @@
 import nltk
+import re
 
 def compress(sent):
-    pos = nltk.pos_tag(sent)
+    sent = re.sub(', age [0-9]+,', '', sent)
+    words = nltk.word_tokenize(sent)
+    pos = nltk.pos_tag(words)
+    if pos[0][1] == 'CC' or pos[0][1] == 'IN':
+        pos = pos[1:]
+    new_sent = []
+    for word in pos:
+        if word[1] != 'RB' and word[1] != 'JJ':
+            new_sent.append(word[0])
+    proc_sent = ' '.join(new_sent)
+    return proc_sent
 
-    return sent
-
-print(compress(['this is a test']))
+print(compress('the brown dog, age 12, runs quickly'))
