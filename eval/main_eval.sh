c39080b8e3c5b4bc14c22253b018fc1c8da30662
mblac6 <mblac6@uw.edu>
2017-05-26 13:18:15 -0700

    devtest summaries

--- eval/main_eval.sh
+++ eval/main_eval.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python3 ../src/main.py ../src/data/new_human.processed.json ../src/data/new_training.processed.json ../src/data/devtest.json ../src/data/devtest.processed.json
-./ROUGE-1.5.5.pl -e data -a -n 4 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d ../eval/test.xml
+python3 ../src/main.py ../src/data/new_human.processed.json ../src/data/new_training.processed.json ../src/data/evaltest.json ../src/data/evaltest.processed.json
+./ROUGE-1.5.5.pl -e data -a -n 4 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d ../eval/evaltest.xml
